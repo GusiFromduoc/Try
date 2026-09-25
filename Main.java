36999d0631573515b8e9a86bd7c11288db2a69d8
GusiFromduoc <gus.gonzalezg@duocuc.cl>
2026-09-25 14:36:22 -0300

    nada

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String [] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese su edad por favor");
+
+        int edad = scanner.nextInt();
+
+        System.out.println("Edad registrada: " + edad);
+
+
+    }
+}
