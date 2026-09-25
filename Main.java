39d6eda622911656e15043b6d717b502af78095e
GusiFromduoc <gus.gonzalezg@duocuc.cl>
2026-09-25 16:25:17 -0300

    ahora sí

--- Main.java
+++ Main.java
@@ -8,7 +8,7 @@ public class Main {
         System.out.println("Ingrese su nombre por favor: ");
         String nombre = scanner.nextLine();
 
-
+        //Aquí hacemos el try de lo que queremos conseguir
         try {
             System.out.println("Ingrese su edad por favor: ");
             int edad = scanner.nextInt();
@@ -17,7 +17,9 @@ public class Main {
             System.out.println("Edad registrada: " + edad + "%nAcceso autorizado.");} else {
                 System.out.println("Acceso restringido. ");
             }
-
+        /* Aquí va el catch en caso de que ocurra el error de arriba lo atrape 
+        el InputMismatchException, así no arruinar el proceso y que le podamos
+        entregar un mensaje a la persona de qué error cometió.*/
         }
         catch (InputMismatchException e) {
                 System.out.println("Por favor ingrese una edad correcta.");
