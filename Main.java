02a1da3f6e0a9ae5984a6f6c87d8096133e8435e
GusiFromduoc <gus.gonzalezg@duocuc.cl>
2026-09-25 14:54:13 -0300

    c

--- Main.java
+++ Main.java
@@ -5,11 +5,18 @@ public class Main {
     public static void main(String [] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Ingrese su nombre por favor: ");
+        String nombre = scanner.nextLine();
+
 
         try {
-            System.out.println("Ingrese su edad por favor");
+            System.out.println("Ingrese su edad por favor: ");
             int edad = scanner.nextInt();
-            System.out.println("Edad registrada: " + edad);
+
+            if (edad >= 18) {
+            System.out.println("Edad registrada: " + edad + "%nAcceso autorizado.");} else {
+                System.out.println("Acceso restringido. ");
+            }
 
         }
         catch (InputMismatchException e) {
